7140a5586e69cbf5f77dacbcd2158777a326618a
ctcpip <ctcpip@users.noreply.github.com>
2024-05-01 14:12:35 -0500

    🐛 fix file path

--- src/pkgJSON.mjs
+++ src/pkgJSON.mjs
@@ -1,4 +1,8 @@
 import { readFileSync } from 'node:fs';
+import { cwd as getCWD } from 'node:process';
+import path from 'node:path';
+
+const cwd = getCWD();
 
 // import attributes when?
-export default JSON.parse(readFileSync('package.json', 'utf8').toString());
+export default JSON.parse(readFileSync(path.join(cwd, 'package.json'), 'utf8').toString());
