d32204998a570d77613643eb185728c91df13cbb
ctcpip <ctcpip@users.noreply.github.com>
2025-08-14 11:19:10 -0500

    ⬆️ upgrade deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
 import ultraMegaConfig from 'eslint-config-ultra-mega';
+import globals from 'globals';
 
 export default [
   ...ultraMegaConfig,
+  { languageOptions: { globals: { ...globals.node } } },
 ];
